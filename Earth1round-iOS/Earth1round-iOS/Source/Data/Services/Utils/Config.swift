bf90ac7245fd2aa813890f1920730867db18c6b9
kong <1018dbrud@gmail.com>
2022-08-16 18:18:09 +0900

    [add] moya + 서버 연결 세팅

---
+++ Earth1round-iOS/Earth1round-iOS/Source/Data/Services/Utils/Config.swift
@@ -0,0 +1,18 @@
+//
+//  Config.swift
+//  Earth1round-iOS
+//
+//  Created by kong on 2022/08/14.
+//
+
+import Foundation
+
+struct Config {
+    
+    enum Network {
+        static var baseURL: String {
+            return "3.37.73.80:8082"
+        }
+    }
+    
+}
